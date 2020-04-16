eb23602c1c8099b9d2b9556d3c6313a85bd98dd5
Gyusun Yeom <omniavinco@gmail.com>
2020-04-16 22:42:30 +0900

    Clean dist before build

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,19 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import path from 'path';
 
+const isProduction = process.env['NODE_ENV'] === 'production';
+
 const config: webpack.Configuration = {
-    mode: 'development',
+    mode: isProduction ? 'production' : 'development',
     entry: path.join(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src', 'index.html'),
         }),
