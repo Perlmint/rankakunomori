51f348f08e9a599ff48df163272ed0f4d180b515
Gyusun Yeom <omniavinco@gmail.com>
2020-04-18 20:04:51 +0900

    Updates

--- 1.bundle.js
+++ 1.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{39:function(n){n.exports=JSON.parse('{"en":"English","ko":"Korean (한국어)","ja":"Japanese (日本語)"}')}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{39:function(e){e.exports=JSON.parse('{"en":"English","ko":"Korean (한국어)","ja":"Japanese (日本語)","fossil.usage":"{x}: have never been found.\\t{check}: exihibited in museum.\\t{number}any number: remain things"}')}}]);
